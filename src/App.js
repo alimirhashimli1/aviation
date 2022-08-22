08f1f4d099fc063a24c21e2b15d60d4bb0f47508
alimirhashimli <alimirhashimli@gmail.com>
2022-08-22 23:15:26 +0200

    my commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
 import logo from './logo.svg';
+import {useEffect, useState} from "react";
 import './App.css';
+import axios from "axios";
+import { data } from './data';
 
 function App() {
+  const [aviationId, setAviationId ] = useState([])
+  const [checkedInput, setCheckedInput] = useState(false)
+  
+  useEffect(()=>{
+    for(let i = 0; i < data.data.airlines.length; i++){
+      setAviationId(aviationId+1)
+    }
+  
+  }, [])
+
+  const handleChange = (event, index) =>{
+    setCheckedInput(checkedInput => !checkedInput)
+    console.log(event.target.className)
+   if(aviationId[index] === index){
+
+    return event.target.checked
+   } else {
+    return !event.target.checked
+   }
+  }
+
+  
+
+  const settings = {
+    method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer wxq3fba4l0bppt0gx4oq5nyexconk03",
+        "Accept": "application/vnd.aviation.v1+json"
+      
+    },
+    credentials: "include"
+
+  }
+
+  
+   
+    
+  
+
+
+
+useEffect(()=>{
+    axios.get("https://aviation.technoly.com/api/airline")
+    .then(res => {
+      console.log(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    }
+    )
+  }, [])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+      <thead>
+            <tr>
+              <th>Airline Name</th>
+              <th>Airline Callsign</th>
+              <th>Checked</th>
+            </tr>
+        </thead>
+        <tbody >
+      {
+        Object.keys(data.data.airlines).map((airline,index) => (
+           <tr className={checkedInput ? "active-row" : ""} key={index}>
+            <td>
+           <h2>{data.data.airlines[airline].name}</h2>
+           </td>
+           <td >
+          <h3>{data.data.airlines[airline].callsign}</h3>
+          </td>
+          <td >
+          <input id={index} type="checkbox" name={data.data.airlines[airline].name} onChange={(event)=> {
+            handleChange(event, aviationId[index])            
+          }} />
+          </td>
+          </tr>
+          
+         ))
+        }
+        </tbody>
+        </table>
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,55 @@
+export const data = {
+
+    meta: {
+        success: "true",
+        filter: {
+            since: "2001-10-01 10:01:01"
+        },
+        results: 5
+    },
+    data: {
+        airlines: {
+            FDM: {
+                iata: "",
+                icao: "FDM",
+                name: "Freedom Air",
+                callsign: "FREEDOM",
+                shutdown: false,
+                updated: "2022-06-22 15:11:09"
+            },
+            BID: {
+                iata: "",
+                icao: "BID",
+                name: "Bin Air",
+                callsign: null,
+                shutdown: false,
+                updated: "2022-03-11 09:57:57"
+            },
+            ENT: {
+                iata: "",
+                icao: "ENT",
+                name: "Enter Air",
+                callsign: "ENTER",
+                shutdown: false,
+                updated: "2022-03-26 10:29:50"
+            },
+            BWI: {
+                iata: "",
+                icao: "BWI",
+                name: "Blue Wing Airlines",
+                callsign: "",
+                shutdown: false,
+                updated: "2022-04-11 12:43:08"
+            },
+            MAG: {
+                iata: "",
+                icao: "MAG",
+                name: "Meridian Airways",
+                callsign: "MERID AIR",
+                shutdown: true,
+                updated: "2022-04-11 12:54:52"
+            }
+        }
+    }
+}
+
